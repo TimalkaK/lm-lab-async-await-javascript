43cbea61001f91d03e9039e210523a93510ae285
TimalkaK <timalkamk@gmail.com>
2023-07-31 11:35:43 +0100

    Feat: Completed Extension in Exercise 2

--- exercise002.js
+++ exercise002.js
@@ -1,20 +1,34 @@
 import fetch from "node-fetch";
 
-const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+// const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-// const fetchData = (apiEndPoint) => {
-//   fetch(apiEndPoint)
-//     .then((response) => response.json())
-//     .then((json) => console.log(json))
-//     .catch((error) => console.log(error));
-// };
+// // const fetchData = (apiEndPoint) => {
+// //   fetch(apiEndPoint)
+// //     .then((response) => response.json())
+// //     .then((json) => console.log(json))
+// //     .catch((error) => console.log(error));
+// // };
+
+// // fetchData(jsonTypicode);
+
+// async function fetchData(apiEndPoint){
+//   try{
+//     const response = await fetch(apiEndPoint);
+//     const json = await response.json();
+//     console.log(json);
+//   }catch(error){
+//     console.log(error);
+//   }
+// }
 
 // fetchData(jsonTypicode);
 
 
-async function fetchData(apiEndPoint){
+const catFacts = "https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=2";
+
+async function fetchData(catFacts){
   try{
-    const response = await fetch(apiEndPoint);
+    const response = await fetch(catFacts);
     const json = await response.json();
     console.log(json);
   }catch(error){
@@ -22,4 +36,4 @@ async function fetchData(apiEndPoint){
   }
 }
 
-fetchData(jsonTypicode);
+fetchData(catFacts);
